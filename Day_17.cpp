0750fa3ff5933fa7f775e95dc9cd80c52266ca46
Pratik Mishra <pratikmishra4600@gmail.com>
2025-08-31 22:54:22 +0530

    Create Day_17.cpp
    Task of finding the Prime Factorization of a Number completed

---
+++ Day_17.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> primeFactorization(int n) {
+    vector<int> factors;
+    while (n%2==0) {
+        factors.push_back(2);
+        n /= 2;
+    }
+    for (int i=3; i*i<=n; i+=2) {
+        while (n%i==0) {
+            factors.push_back(i);
+            n/=i;
+        }
+    }
+    if (n>2) {
+        factors.push_back(n);
+    }
+    return factors;
+}
+int main() {
+    int m;
+    cout<<"Enter a number: ";
+    cin>>m;
+    vector<int> result = primeFactorization(m);
+    cout<<"Prime factors: [ ";
+    for (int val : result) {
+        cout<<val<< " ";
+    }
+    cout<<"]"<<endl;
+    return 0;
+}
