f8c50678ab7579d742664f931e404db80c998fdf
Pratik Mishra <pratikmishra4600@gmail.com>
2025-09-02 22:31:14 +0530

    Create Day_19.cpp
    Task to Evaluate a PostFix Expression completed

---
+++ Day_19.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<stack>
+#include<string>
+#include<vector>
+using namespace std;
+
+int evaluatePostfix(vector<string>& tokens) {
+    stack<int> st;
+    for (string& token : tokens) {
+        if (token == "+" || token == "-" || token == "*" || token == "/") {
+            int b = st.top(); st.pop();
+            int a = st.top(); st.pop();
+            if (token == "+") st.push(a + b);
+            else if (token == "-") st.push(a - b);
+            else if (token == "*") st.push(a * b);
+            else st.push(a / b); 
+        } else {
+            st.push(stoi(token));
+        }
+    }
+    return st.top();
+}
+
+int main() {
+    vector<string> tokens = {"2", "1", "+", "3", "*"}; 
+    cout<<evaluatePostfix(tokens)<<endl;
+    return 0;
+}
