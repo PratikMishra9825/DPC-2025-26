a8f4befc683359e04a04baeb784c1cba5ece2422
Pratik Mishra <pratikmishra4600@gmail.com>
2025-09-04 21:52:41 +0530

    Update Day_21.cpp
    Task of Reverse a Stack Using Recursion completed

--- Day_21.cpp
+++ Day_21.cpp
@@ -2,37 +2,44 @@
 #include <vector>
 #include <stack>
 using namespace std;
-
 void insertAtBottom(stack<int>& st, int x) {
     if(st.empty()) {
         st.push(x);
         return;
     }
-    int top=st.top();
+    int topElem=st.top();
     st.pop();
     insertAtBottom(st, x);
-    st.push(top);
+    st.push(topElem);
 }
 void reverseStack(stack<int>& st) {
-    if(st.empty())
-     return;
-
-    int top=st.top();
+    if(st.empty()) 
+    return;
+    int topElem=st.top();
     st.pop();
-    reverseStack(st);   
-    insertAtBottom(st, top);
-}
+    reverseStack(st);        
+    insertAtBottom(st, topElem); 
+    }
+
 int main() {
     stack<int> st;
     st.push(3);
     st.push(1);
     st.push(4);
     st.push(2);
+    cout << "Original Stack: ";
+    stack<int> temp=st;
+    while(!temp.empty()) {
+        cout<<temp.top()<<" ";
+        temp.pop();
+    }
+    cout << endl;
     reverseStack(st);
-    cout<<"Reversed Stack: ";
+    cout<<"Reversed Stack :";
     while(!st.empty()) {
-        cout<<st.top()<<" ";
+        cout<<st.top()<< " ";
         st.pop();
     }
+    cout<<endl;
     return 0;
 }
